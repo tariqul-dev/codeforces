4f6b178323b27fe3239e9956833c4e5f77587d98
tariqul.islam <tariqulislam.dev@gmail.com>
2023-05-06 10:36:26 +0600

    div 4 practice

---
+++ Contests/Div4(1)/D_Odd_Queries.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* clang-format off */
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+/* FUNCTIONS */
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}";
+}
+/* UTILS */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+ll min(ll a,int b) { if (a<b) return a; return b;
+}
+ll min(int a,ll b) { if (a<b) return a; return b;
+}
+ll max(ll a,int b) { if (a>b) return a; return b;
+}
+ll max(int a,ll b) { if (a>b) return a; return b;
+}
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b);
+}
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b;
+}
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i
+	]>='a' && a[i
+	]<='z') a[i
+	]-='a'-'A'; return a;
+}
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i
+	]>='A' && a[i
+	]<='Z') a[i
+	]+='a'-'A'; return a;
+}
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1;
+}
+void yes() { cout<<"YES\n";
+}
+void no() { cout<<"NO\n";
+}
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+/* clang-format on */
+/* Main()  function */
+int main()
+{
+    int64 t;
+
+    cin >> t;
+
+    while (t--)
+    {
+        int64 n, q;
+        cin >> n >> q;
+        vector<int64> arr;
+        vector<int64> prefixSum;
+        arr.push_back(0);
+        prefixSum.push_back(0);
+        int64 sum = 0;
+
+        for (int64 i = 1; i <= n; i++)
+        {
+            int64 a;
+            cin >> a;
+            arr.push_back(a);
+            sum += a;
+            prefixSum.push_back(sum);
+        }
+
+        while (q--)
+        {
+            int64 l, r, k;
+            cin >> l >> r >> k;
+
+            sum = sum - (prefixSum[r] - prefixSum[l - 1]) + k * (r - l + 1);
+
+            if (sum % 2 != 0)
+                yes();
+            else
+                no();
+
+            sum = prefixSum[prefixSum.size() - 1];
+        }
+    }
+    return 0;
+}
+/* Main() Ends Here */
